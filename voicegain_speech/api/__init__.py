3eeb08bf023c41285ec43500a992bf05a5b873d3
VoicegainGithub <>
2020-08-26 01:33:31 +0000

    "CI"

--- voicegain_speech/api/__init__.py
+++ voicegain_speech/api/__init__.py
@@ -5,6 +5,7 @@ from __future__ import absolute_import
 # import apis into api package
 from voicegain_speech.api.aivr_api import AivrApi
 from voicegain_speech.api.aivr_callback_api import AivrCallbackApi
+from voicegain_speech.api.asr_callback_api import AsrCallbackApi
 from voicegain_speech.api.data_api import DataApi
 from voicegain_speech.api.greg_api import GregApi
 from voicegain_speech.api.lm_api import LmApi
